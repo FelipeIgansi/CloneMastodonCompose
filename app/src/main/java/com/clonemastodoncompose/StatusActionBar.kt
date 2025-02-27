8c725d36a3fd39ee780ba9720874755fec600f3b
FelipeIgansi <lipeigansi@gmail.com>
2025-02-27 13:21:42 -0300

    Some adjustments to keep the StatusActionBar More similar with the original

--- app/src/main/java/com/clonemastodoncompose/StatusActionBar.kt
+++ app/src/main/java/com/clonemastodoncompose/StatusActionBar.kt
@@ -1,18 +1,14 @@
 package com.clonemastodoncompose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -31,117 +27,103 @@ import androidx.compose.ui.unit.dp
 fun StatusActionBar(paddingValues: PaddingValues) {
     Box(
         Modifier
-            .padding(paddingValues)
-            .fillMaxSize(), contentAlignment = Alignment.Center
+            .padding(paddingValues),
+        contentAlignment = Alignment.Center
     ) {
-        // Outer Row container equivalent to a horizontal LinearLayout
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(42.dp)
-                .padding(bottom = 8.dp, start = 8.dp, end = 8.dp)
+                .padding(bottom = 8.dp, start = 8.dp, end = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            // Reply button container (equivalent to FrameLayout with a background)
-            Button(
-                onClick = {},
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.LightGray
-                )
-            ) {
-                // Row to align the icon and text similar to drawableStart
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
+            Row(modifier = Modifier.weight(1f)) {
+                Button(
+                    contentPadding = PaddingValues(5.dp),
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
+                    )
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_reply),
-                        contentDescription = "Reply Icon",
-                        tint = MaterialTheme.colorScheme.outline
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_reply),
+                            contentDescription = "Reply Icon",
+                            tint = MaterialTheme.colorScheme.outline,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Spacer(modifier = Modifier.width(6.dp))
+                        Text(
+                            text = "123",
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.outline
+                        )
+                    }
+                }
+
+
+                Button(
+                    contentPadding = PaddingValues(5.dp),
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
                     )
-                    Spacer(modifier = Modifier.width(6.dp))
-                    Text(
-                        text = "123", // Preview text
-                        style = MaterialTheme.typography.labelMedium,
-                        color = MaterialTheme.colorScheme.outline
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_repeat_selector),
+                            contentDescription = "Boost Icon",
+                            tint = MaterialTheme.colorScheme.outline,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Spacer(modifier = Modifier.width(6.dp))
+                        Text(
+                            text = "123",
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.outline
+                        )
+                    }
+                }
+
+
+                Button(
+                    contentPadding = PaddingValues(5.dp),
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
                     )
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_star_selector),
+                            contentDescription = "Favorite Icon",
+                            tint = MaterialTheme.colorScheme.outline,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Spacer(modifier = Modifier.width(6.dp))
+                        Text(
+                            text = "123", // Preview text
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.outline
+                        )
+                    }
                 }
             }
 
-            // Spacer with weight to distribute extra space
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(1.dp)
-            )
-            // Boost button container
-            Button(
-                onClick = {},
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.LightGray
-                )
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_repeat_selector),
-                        contentDescription = "Boost Icon",
-                        tint = MaterialTheme.colorScheme.outline
-                    )
-                    Spacer(modifier = Modifier.width(6.dp))
-                    Text(
-                        text = "123", // Preview text
-                        style = MaterialTheme.typography.labelMedium,
-                        color = MaterialTheme.colorScheme.outline
-                    )
-                }
-            }
-            // Spacer with weight to distribute extra space
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(1.dp)
-            )
-            // Favorite button container
-
-            Button(
-                onClick = {},
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.LightGray
-                )
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_star_selector),
-                        contentDescription = "Favorite Icon",
-                        tint = MaterialTheme.colorScheme.outline
-                    )
-                    Spacer(modifier = Modifier.width(6.dp))
-                    Text(
-                        text = "123", // Preview text
-                        style = MaterialTheme.typography.labelMedium,
-                        color = MaterialTheme.colorScheme.outline
-                    )
-                }
-            }
-
-            // Spacer with weight to distribute extra space
-            Spacer(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(1.dp)
-            )
-            // Share button container
-
-            IconButton (
+            IconButton(
                 onClick = {},
             ) {
-
                 Icon(
                     painter = painterResource(id = R.drawable.ic_share_20px),
                     contentDescription = "Share Icon",
-                    tint = MaterialTheme.colorScheme.outline
+                    tint = MaterialTheme.colorScheme.outline,
+                    modifier = Modifier.size(20.dp)
                 )
             }
         }
@@ -150,7 +132,7 @@ fun StatusActionBar(paddingValues: PaddingValues) {
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun StatusActioBarPrev() {
     StatusActionBar(PaddingValues())
